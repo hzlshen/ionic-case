f17327346b4e1f3ebce3a63218ffb1a5752ee1d3
hzlshen <m15277968105@163.com>
2017-12-21 17:30:28 +0800

    cards

--- src/pages/cards/cards.ts
+++ src/pages/cards/cards.ts
@@ -1,12 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
-/**
- * Generated class for the CardsPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
 
 @IonicPage()
 @Component({
@@ -15,7 +9,38 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class CardsPage {
 
+  cardItems:any[];
+  
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+    this.cardItems=[
+      {
+        user:{
+          avatar: 'assets/img/marty-avatar.png',
+          name: '大王'
+        },
+        date: 'November 5, 1955',
+        image: 'assets/img/advance-card-bttf.png',
+        content: 'Wait a minute. Wait a minute, Doc. Uhhh... Are you telling me that you built a time machine... out of a DeLorean?! Whoa. This is heavy.',
+      },
+      {
+        user:{
+          avatar: 'assets/img/marty-avatar.png',
+          name: '二王'
+        },
+        date: 'November 5, 1955',
+        image: 'assets/img/advance-card-bttf.png',
+        content: 'Wait a minute. Wait a minute, Doc. Uhhh... Are you telling me that you built a time machine... out of a DeLorean?! Whoa. This is heavy.',
+      },
+      {
+        user:{
+          avatar: 'assets/img/marty-avatar.png',
+          name: '三王'
+        },
+        date: 'November 5, 1955',
+        image: 'assets/img/advance-card-bttf.png',
+        content: 'Wait a minute. Wait a minute, Doc. Uhhh... Are you telling me that you built a time machine... out of a DeLorean?! Whoa. This is heavy.',
+      },
+    ]
   }
 
   ionViewDidLoad() {
