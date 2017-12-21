04dcbf8fa0c718278c7a2be28bbbac206b68d79a
hzlshen <m15277968105@163.com>
2017-12-21 15:30:23 +0800

    signup

--- src/pages/signup/signup.ts
+++ src/pages/signup/signup.ts
@@ -1,12 +1,9 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {IonicPage, NavController, NavParams, ToastController} from 'ionic-angular';
+import { User } from '../../providers/providers';
+import { MainPage } from '../pages';
+import {TranslateService} from "@ngx-translate/core";
 
-/**
- * Generated class for the SignupPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
 
 @IonicPage()
 @Component({
@@ -15,9 +12,41 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class SignupPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  account:{name:string,email:string,password:string}={
+    name:'Hello 老王',
+    email:'test@example.com',
+    password:'test'
+  };
+  
+  private signupErrorString:string;
+  
+  constructor(public navCtrl: NavController,
+              public navParams: NavParams,
+              public user:User,
+              public toastCtrl:ToastController,
+              public translateService:TranslateService
+  ) {
+    this.translateService.get('SIGNUP_ERROR').subscribe((value)=>{
+      this.signupErrorString=value;
+    })
   }
-
+  
+  doSignup(){
+    this.user.signup(this.account).subscribe((resp)=>{
+      this.navCtrl.push(MainPage);
+    },(err)=>{
+      this.navCtrl.push(MainPage);
+      
+      let toast = this.toastCtrl.create({
+        message:this.signupErrorString,
+        duration:3000,
+        position:'top'
+      });
+      toast.present();
+    })
+    
+  }
+  
   ionViewDidLoad() {
     console.log('ionViewDidLoad SignupPage');
   }
