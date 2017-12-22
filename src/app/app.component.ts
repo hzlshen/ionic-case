b189e7ae68c2d2c4fb37199f3732a553bd172c09
hzlshen <m15277968105@163.com>
2017-12-22 17:10:25 +0800

    appcomponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,93 @@
-import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
-import { StatusBar } from '@ionic-native/status-bar';
+import { Component, ViewChild } from '@angular/core';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { StatusBar } from '@ionic-native/status-bar';
+import { TranslateService } from '@ngx-translate/core';
+import { Config, Nav, Platform } from 'ionic-angular';
 
-import { TabsPage } from '../pages/tabs/tabs';
+import { FirstRunPage } from '../pages/pages';
+import { Settings } from '../providers/providers';
 
 @Component({
-  templateUrl: 'app.html'
+  template:`
+    <ion-menu [content]="content">
+      <ion-header>
+        <ion-toolbar>
+          <ion-title>Pages</ion-title>
+        </ion-toolbar>
+      </ion-header>
+    
+      <ion-content>
+        <ion-list>
+          <button menuClose ion-item *ngFor="let p of pages" (click)="openPage(p)">
+            {{p.title}}
+          </button>
+        </ion-list>
+      </ion-content>
+  
+    </ion-menu>
+    <ion-nav #content [root]="rootPage"></ion-nav>`
 })
 export class MyApp {
-  rootPage:any = TabsPage;
-
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  rootPage:any = FirstRunPage;
+  
+  @ViewChild(Nav) nav:Nav;
+  
+  pages:any[]=[
+    {title:'Tutorial',component:"TutorialPage"},
+    {title:'Welcome',component:"WelcomePage"},
+    {title:'Tabs',component:"TabsPage"},
+    {title:'Cards',component:"CardsPage"},
+    {title:'Content',component:"ContentPage"},
+    {title:'Login',component:"LoginPage"},
+    {title:'Signup',component:"SignupPage"},
+    {title:'Master Detail',component:"ListMasterPage"},
+    {title:'Menu',component:"MenuPage"},
+    {title:'Settings',component:"SettingsPage"},
+    {title:'Search',component:"SearchPage"},
+  ];
+  
+  constructor(
+    private translate:TranslateService,
+    private config:Config,
+    settings:Settings,
+    platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
     });
+    this.initTranslate();
   }
+  
+  initTranslate() {
+    this.translate.setDefaultLang('en');
+    const browserLang = this.translate.getBrowserLang();
+    
+    if (browserLang) {
+      if (browserLang === 'zh') {
+        const browserCultureLang = this.translate.getBrowserCultureLang();
+        
+        if (browserCultureLang.match(/-CN|CHS|Hans/i)) {
+          this.translate.use('zh-cmn-Hans');
+        } else if (browserCultureLang.match(/-TW|CHT|Hant/i)) {
+          this.translate.use('zh-cmn-Hant');
+        }
+      } else {
+        this.translate.use(this.translate.getBrowserLang());
+      }
+    } else {
+      this.translate.use('en'); // Set your language here
+    }
+    
+    this.translate.get(['BACK_BUTTON_TEXT']).subscribe(values => {
+      this.config.set('ios', 'backButtonText', values.BACK_BUTTON_TEXT);
+    });
+  }
+  
+  openPage(page) {
+    this.nav.setRoot(page.component);
+  }
+  
+  
 }
--- src/pages/settings/settings.ts
+++ src/pages/settings/settings.ts
@@ -1,12 +1,9 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {FormBuilder, FormGroup} from "@angular/forms";
+import {TranslateService} from "@ngx-translate/core";
 
-/**
- * Generated class for the SettingsPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+
 
 @IonicPage()
 @Component({
@@ -15,11 +12,78 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class SettingsPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  options:any;
+  
+  settingsReady =false;
+  
+  form:FormGroup;
+  
+  profileSettings ={
+    page:'profile',
+    pageTitleKey:'SETTINGS_PAGE_PROFILE'
+  };
+  
+  page:string = 'main';
+  pageTitleKey :string = 'SETTINGS_TITLE';
+  pageTitle:string;
+  
+  subSettings: any = SettingsPage;
+  
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    public settings:SettingsPage,
+    public formBuilder:FormBuilder,
+    public translate:TranslateService
+  ) {
+  }
+  
+  _buildForm(){
+    let group:any={
+      option1:[this.options.option1],
+      option2:[this.options.option2],
+      option3:[this.options.option3],
+    };
+    
+    switch (this.page){
+      case 'main':
+        break;
+      case 'profile':
+        group={
+          option4:[this.options.option4]
+        };
+        break;
+    }
+    this.form = this.formBuilder.group(group);
+  
+    this.form.valueChanges.subscribe((v) => {
+      this.settings.merge(this.form.value);
+    });
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad SettingsPage');
+    this.form = this.formBuilder.group({});
+  }
+  ionViewWillEnter() {
+    this.form = this.formBuilder.group({});
+    
+    this.page = this.navParams.get('page') || this.page;
+    this.pageTitleKey = this.navParams.get('pageTitleKey') || this.pageTitleKey;
+    
+    this.translate.get(this.pageTitleKey).subscribe((res) => {
+      this.pageTitle = res;
+    })
+    
+    this.settings.load().then(() => {
+      this.settingsReady = true;
+      this.options = this.settings.allSettings;
+      
+      this._buildForm();
+    });
+  }
+  
+  ngOnChanges() {
+    console.log('Ng All Changes');
   }
 
 }
